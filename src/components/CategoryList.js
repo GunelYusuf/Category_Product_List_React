e67185c2a383475c6b7ecdda609862c4dcd22383
GunelYusuf <91114321+GunelYusuf@users.noreply.github.com>
2022-02-26 20:12:50 +0400

    feat:create state

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,51 +1,33 @@
 import React, {Component} from 'react';
 import {ListGroup,ListGroupItem} from 'reactstrap';
 
+
 class CategoryList extends Component {
     constructor(props) {
         super(props);
-        state:{}
-    }
+        this.state={
+            categories:[
+                {categoryId:1,categoryName:'Beverages'},
+                {categoryId:2,categoryName:'Condiments'},
+                {categoryId:3,categoryName:'Clothes'
+                }
+            ]
+        }
+    };
     render() {
         return (
             <div>
                 <h2>{this.props.info.title}</h2>
                 <h4>{this.props.info.test}</h4>
                   <ListGroup>
-                        <ListGroupItem
-                            action
-                            active
-                            tag="button"
-                        >
-                            Cras justo odio
-                        </ListGroupItem>
-                        <ListGroupItem
-                            action
-                            tag="button"
-                        >
-                            Dapibus ac facilisis in
-                        </ListGroupItem>
-                        <ListGroupItem
-                            action
-                            tag="button"
-                        >
-                            Morbi leo risus
-                        </ListGroupItem>
-                        <ListGroupItem
-                            action
-                            tag="button"
-                        >
-                            Porta ac consectetur ac
-                        </ListGroupItem>
-                        <ListGroupItem
-                            action
-                            disabled
-                            tag="button"
-                        >
-                            Vestibulum at eros
-                        </ListGroupItem>
-                    </ListGroup>
-            </div>
+                      {this.state.categories.map(category =>
+                          <ListGroupItem
+                            key={category.categoryId}
+                          >
+                              {category.categoryName}
+                          </ListGroupItem>)}
+                 </ListGroup>
+           </div>
         );
     }
 }
